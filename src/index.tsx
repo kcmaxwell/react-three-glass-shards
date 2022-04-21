920b26aaff0800811dec3352f97be8c6968ef5f8
Kristopher Maxwell <zerotrig@gmail.com>
2022-04-20 20:02:26 -0600

    Add skybox, metal box, restructure files

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,48 @@
-import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import "./styles.css";
+import * as THREE from "three";
+import { Canvas, useFrame, MeshProps } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
+import { Environment, OrbitControls } from "@react-three/drei";
+import FPSStats from "react-fps-stats";
+import ReactDOM from "react-dom";
 
-import App from "./App";
+const Box = (props: MeshProps) => {
+  const mesh = useRef<THREE.mesh>();
 
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += 1 * delta;
+    mesh.current.rotation.y += 1 * delta;
+  });
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  return (
+    <mesh {...props} ref={mesh} scale={[1, 1, 1]}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshPhysicalMaterial metalness={1} roughness={0} />
+    </mesh>
+  );
+};
+
+ReactDOM.render(
+  <div className="App">
+    <FPSStats />
+    <Canvas>
+      <Suspense fallback={"Loading..."}>
+        <Box />
+        <pointLight position={[10, 10, 10]} />
+        <ambientLight />
+        <Environment
+          files={[
+            "/skybox/posx.jpg",
+            "/skybox/negx.jpg",
+            "/skybox/posy.jpg",
+            "/skybox/negy.jpg",
+            "/skybox/posz.jpg",
+            "/skybox/negz.jpg"
+          ]}
+          background
+        />
+      </Suspense>
+    </Canvas>
+  </div>,
+  document.getElementById("root")
 );
